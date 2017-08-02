bf8c186e007344c026dcb4b1f026e7fc64f7d407
Joe Shepherd <joeshepmedia@gmail.com>
2017-08-01 21:25:56 -0500

    Add plant images; render diff images

--- app/app.js
+++ app/app.js
@@ -4,11 +4,10 @@ let todoApp = angular.module("TodoApp", ["ngRoute"])
 .constant("FirebaseUrl", "https://d20-ng-todo.firebaseio.com/");
 
 todoApp.controller('gardenCtrl', function($scope, $http, FirebaseUrl){
-  let canvas = document.getElementById("graphCanvas");
-  let context = canvas.getContext("2d");
-  context.lineWidth = 2;
+  // let canvas = document.getElementById("graphCanvas");
+  // let canvas = document.createElement("canvas");
   // Getting jshint warning about leaking var (canvas) here
-  let img = canvas = document.getElementById("img1");
+
   let position;
   let counter = 0;
   let garden = {};
@@ -29,17 +28,18 @@ todoApp.controller('gardenCtrl', function($scope, $http, FirebaseUrl){
   $scope.drop = (ev) => {
       counter ++;
       ev.preventDefault();
-      let offset = ev.dataTransfer.getData("text/plain").split(',');
+      // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer
+      // Basically is grabbing the id string of the image
       let data = ev.dataTransfer.getData("Text");
-      console.log("data", data);
-
+      console.log("data?", data);
+      let img = document.getElementById(data);
+      console.log("img", img);
       // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/offsetLeft
       let dx = position[0] - img.offsetLeft;
       let dy = position[1] - img.offsetTop;
-      document.getElementById("graphCanvas").getContext("2d").drawImage(document.getElementById(data), ev.pageX - dx, ev.pageY - dy);
+      document.getElementById("graphCanvas").getContext("2d").drawImage(img, ev.pageX - dx, ev.pageY - dy);
       console.log("New position?", ev.pageX, ev.pageY);
-      console.log("graphCanvas", document.getElementById("graphCanvas"));
-      garden[`plant${counter}`] = {xCoord: ev.pageX, yCoord: ev.pageY};
+      garden[`plant${counter}`] = {xCoord: ev.pageX, yCoord: ev.pageY, imgName: img.id};
   };
 
   $scope.saveGarden = () => {
@@ -67,7 +67,10 @@ todoApp.controller('gardenCtrl', function($scope, $http, FirebaseUrl){
     let gardenObjects = garden[Object.keys(garden)[0]];
     let canvas = document.getElementById("graphCanvas2");
     for( let plant in gardenObjects ) {
-      console.log("garden[plant].xCoord", plant);
+      let imgName = gardenObjects[plant].imgName;
+      console.log("img", imgName);
+      let img = document.getElementById(imgName);
+      console.log("img???", img);
       canvas
       .getContext("2d").drawImage(img, gardenObjects[plant].xCoord, gardenObjects[plant].yCoord);
     }
